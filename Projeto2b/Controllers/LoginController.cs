c1c4431d37dc210073458e440da15e4ac08ee53f
Renato <renatoaugustosa@gmail.com>
2025-03-21 21:21:34 -0300

    Criando a lógica de login e cadastro

--- Projeto2b/Controllers/LoginController.cs
+++ Projeto2b/Controllers/LoginController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Projeto2b.Repositorio;
+using Projeto2b.Models;
 
 namespace Projeto2b.Controllers
 {
@@ -22,6 +23,19 @@ namespace Projeto2b.Controllers
         [HttpPost]
         public IActionResult Login(string email, string senha)
         {
+            //Ele usa _usuarioRepositorio.ObterUsuario(email) para buscar o usuário no banco de dados.
+            var usuario = _usuarioRepositorio.ObterUsuario(email);
+
+            //Se o usuário for encontrado e a senha estiver correta, ele redireciona o usuário para a página inicial (RedirectToAction("Index", "Home")).
+            if (usuario != null && usuario.Senha == senha)
+            {
+                // Autenticação bem-sucedida
+                return RedirectToAction("Index", "Home");
+            }
+            //Caso contrário, ele adiciona um erro ao ModelState e retorna a visualização de login, exibindo a mensagem de erro.
+            ModelState.AddModelError("", "Email ou senha inválidos.");
+
+            //retorna a visualização (a página HTML) associada a este método
             return View();
         }
 
@@ -32,9 +46,20 @@ namespace Projeto2b.Controllers
         }
 
         [HttpPost]
-        public IActionResult Cadastro()
+        public IActionResult Cadastro(Usuario usuario)
         {
-            return View();
+            //verifica se os dados do usuário são válidos (de acordo com as regras de validação definidas na classe Usuario).
+            if (ModelState.IsValid)
+            {
+                // Se os dados forem válidos, ele usa _usuarioRepositorio.
+                // AdicionarUsuario(usuario) para adicionar o novo usuário ao banco de dados e redireciona para a página de login.
+                _usuarioRepositorio.AdicionarUsuario(usuario);
+
+                // visualização de cadastro, exibindo os erros de validação
+
+                return RedirectToAction("Login");
+            }
+            return View(usuario);
         }
     }
 }
